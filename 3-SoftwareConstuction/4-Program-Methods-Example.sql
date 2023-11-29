761d372cd4fd13353789915f8664ea4dccbbbb38
Yaroslav Omelchuk <90680719+fluoritka@users.noreply.github.com>
2023-11-29 18:00:31 +0200

    Update 4-Program-Methods-Example.sql

--- 3-SoftwareConstuction/4-Program-Methods-Example.sql
+++ 3-SoftwareConstuction/4-Program-Methods-Example.sql
@@ -2,12 +2,14 @@ CREATE OR REPLACE FUNCTION createRequest (
     p_fullName IN VARCHAR2,
     p_address IN VARCHAR2,
     p_age IN INT,
+    p_gender IN VARCHAR2,
     p_psychosvcName IN VARCHAR2
 )
 RETURN INT IS
     v_fullName "User".fullName%TYPE := TRIM(p_fullName);
     v_address "User".address%TYPE := TRIM(p_address);
     v_age "User".age%TYPE := p_age;
+    v_gender "User".gender%TYPE := TRIM(UPPER(p_gender));
     v_psychosvcName Psychological_service.psychosvcName%TYPE := TRIM(p_psychosvcName);
 BEGIN
     -- Умова №1: Повне ім'я користувача не повинно бути порожнім і не повинно перевищувати 120 символів.
@@ -25,14 +27,19 @@ BEGIN
         RETURN -3; -- Помилка: Вік користувача не відповідає умовам.
     END IF;
 
-    -- Умова №4: Назва психологічної послуги не повинна бути порожньою і не повинна перевищувати 100 символів.
+    -- Умова №4: Стать користувача повинна відповідати одному з варіантів: "чоловік", "жінка", "інше".
+    IF v_gender NOT IN ('ЧОЛОВІК', 'ЖІНКА', 'ІНШЕ') THEN
+        RETURN -4; -- Помилка: Стать користувача не відповідає умовам.
+    END IF;
+
+    -- Умова №5: Назва психологічної послуги не повинна бути порожньою і не повинна перевищувати 100 символів.
     IF LENGTH(v_psychosvcName) = 0 OR LENGTH(v_psychosvcName) > 100 THEN
-        RETURN -4; -- Помилка: Назва психологічної послуги не відповідає умовам.
+        RETURN -5; -- Помилка: Назва психологічної послуги не відповідає умовам.
     END IF;
 
     -- Всі умови виконані, можна вставляти дані в таблицю.
-    INSERT INTO "User" (fullName, address, age, psychosvcName)
-    VALUES (v_fullName, v_address, v_age, v_psychosvcName);
+    INSERT INTO "User" (fullName, address, age, gender, psychosvcName)
+    VALUES (v_fullName, v_address, v_age, v_gender, v_psychosvcName);
     COMMIT;
 
     RETURN 1; -- Користувач успішно ввів всі дані.
